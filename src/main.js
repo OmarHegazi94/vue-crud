8ed9a0e2b422c093de518b9a8ac740c54ee3b271
Omar Hegazy <Omar.Hegazy@intelsoftsystems.com>
2021-08-05 19:51:52 +0200

    finished add post form validation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+// import { VuelidatePlugin } from "@vuelidate/core";
+
 import "bootstrap/dist/css/bootstrap.css";
 
 const app = createApp(App);
 
-app.use(router);
+app.use(router)
+// .use(VuelidatePlugin);
 
 app.mount("#app");
